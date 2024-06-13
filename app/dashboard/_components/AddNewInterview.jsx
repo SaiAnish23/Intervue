35220e4e33834597feba4342ed059f12e0b865ae
SaiAnish23 <anish231003@gmail.com>
2024-06-13 12:23:27 +0530

    feat:Added the AddInterViw component

---
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -0,0 +1,226 @@
+"use client"
+
+import React , {useState} from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from  '../../../components/ui/dialog';
+
+  import {Button} from '../../../components/ui/button'
+
+  import { Input } from "../../../components/ui/input"
+
+  import { Textarea } from "../../../components/ui/textarea"
+  import { run } from '../../../utils/GeminiModel'
+  import { chatSession } from '../../../utils/GeminiModel'
+import { LoaderCircle } from 'lucide-react';
+
+import { db } from '../../../utils/db'
+import { uuid } from 'drizzle-orm/pg-core';
+import { mockInterview } from '../../../utils/schema';
+import { useUser } from '@clerk/nextjs';
+import moment from 'moment';
+import {v4 as uuidv4} from 'uuid'
+ 
+ 
+
+
+function AddNewInterview() {
+
+    const [dialogOpen, setDialogOpen] = useState(false)
+
+    const [jobPosition, setJobPosition] = useState()
+    const [jobDescription, setJobDescription] = useState()
+    const [yearsOfExperience, setYearsOfExperience] = useState()
+    const [loading, setLoading] = useState(false)
+    const [jsonResp, setJsonResp] = useState()
+    const {user} = useUser()
+
+
+     const handleSubmit = async (e) => { 
+          e.preventDefault()
+          setLoading(true)
+
+          // console.log(jobPosition, jobDescription, yearsOfExperience)
+
+       let prompt =   "Given the job position as '" + jobPosition + "', the job description as '" + jobDescription + "', and the required years of experience as '" + yearsOfExperience + "', please generate five interview questions and their corresponding answers in JSON format. The questions should be tailored to understand the candidate's skills, experiences, and fit for the role."
+        // console.log(prompt)
+        // const result =  run(prompt)
+        const result = await chatSession.sendMessage(prompt);
+
+        // console.log(result.response.text())
+
+        const MockJsonResp = (result.response.text()).replace('```json' , '').replace('```' , '')
+        // console.log(MockJsonResp)
+        const jsonResp = JSON.parse(MockJsonResp)
+        console.log(jsonResp)
+        setJsonResp(jsonResp)
+
+         
+           if(jsonResp){
+
+         const resp = await  db.insert(mockInterview).values({
+          mockId :  uuidv4(),
+          jsonMockResp : JSON.stringify(jsonResp),
+          jobPosition : jobPosition,
+          jobDesc : jobDescription,
+          jobExp : yearsOfExperience,
+          createdBy : user?.primaryEmailAddress?.emailAddress,
+          createdAt : moment().format('DD-MM-YYYY')
+
+         }).returning({mockId:mockInterview.mockId})
+
+          console.log(resp)
+
+        } else {
+          console.log('Error')
+        }
+
+
+
+        setLoading(false)
+     }
+  return (
+    <div>
+
+             <div
+             className='bg-white p-10 border shadow-md cursor-pointer hover:shadow-lg rounded-lg text-center hover:scale-105'
+              onClick={() => setDialogOpen(true)}
+             >
+
+                <h2 className="font-bold">
+
+                    + Add New Interview
+                </h2>
+             </div>
+
+
+
+             <Dialog 
+             open={dialogOpen}
+             >
+  
+  <DialogContent  
+   className='max-w-2xl'
+  >
+    <DialogHeader>
+      <DialogTitle
+      className='text-2xl'
+      >
+              Tell us moore about your job Interviewing</DialogTitle>
+      <DialogDescription>
+
+
+        <form
+        
+        onSubmit={handleSubmit}
+        >
+                    
+        <div>
+          
+
+          <h2>
+            Add Detials about your job position/role , job description and years of experience
+          </h2>
+
+
+          <div
+          className='mt-7 my-2'
+          >
+            <label htmlFor="">
+              Job Position/Role
+              <Input placeholder="Ex . Full stack developer"   required 
+              onChange={(e) => setJobPosition(e.target.value)}
+              />
+            </label>
+          </div>
+
+
+
+          <div
+          className=' my-3'
+          >
+            <label htmlFor="">
+              Job Description / Tech Stack(In short)
+              <Textarea placeholder="Ex . React , Nodejs , Springboot" required
+              onChange={(e) => setJobDescription(e.target.value)}
+              />
+            </label>
+          </div>
+
+
+
+
+
+
+          <div
+          className=' my-3'
+          >
+            <label htmlFor="">
+              Tears of Experienc
+              <Input type="number" placeholder="Ex.5" required
+              onChange={(e) => setYearsOfExperience(e.target.value)}
+              />
+            </label>
+          </div>
+
+       </div>
+
+
+
+       <div
+        className='flex justify-end'
+        >
+            <Button
+            className='bg-destructive mt-2'
+            onClick={() => setDialogOpen(false)}
+            >
+                Cancel
+            </Button>
+
+
+            <Button
+            className='bg-primary mt-2 mx-2'
+            type = "submit" 
+            disabled={loading}
+            >
+               {
+                loading ?  
+                <>
+                <LoaderCircle 
+                 className='animate-spin'
+                /> 'Generating Questions' </> : 'Start Interview'
+                 
+               }
+              Start Interview
+
+            </Button>
+        </div>
+
+
+
+
+        </form>
+    
+          
+
+
+
+
+
+
+  
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
+
+    </div>
+  )
+}
+
+export default AddNewInterview
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import { UserButton } from '@clerk/nextjs'
+// import { UserButton } from '@clerk/nextjs'
+import AddNewInterview from './_components/AddNewInterview';
 
 function page() {
   return (
-    <div> page
+    <div
+    className='p-10'
+    > 
+
+      <h2
+      className='text-2xl font-bold text-black'
+      >
+        Dashboard
+      </h2>
+        <h2
+         className='text-gray-500'
+        >
+          Create Ai Mockup Interview
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 my-5">
+          <AddNewInterview/>
+  
+        </div>
+
 
     </div>
   )
